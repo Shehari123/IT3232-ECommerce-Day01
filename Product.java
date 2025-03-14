a839b914497e4063991550e2597f38cf2623019c
Shehari123 <rathnayakeshehari@gmail.com>
2025-03-14 10:58:53 +0000

    update Product.java

--- Product.java
+++ Product.java
@@ -1,3 +1,10 @@
+/*8) implement classes for the given scenario:
+	- A reatail store which can store,edit,and delete 
+multible products like fruits, vegetables, grocery, etc..
+	- display the avilabe products like price, name, 
+	quantity */
+	
+
 class Product {
     String name;
     double price;
