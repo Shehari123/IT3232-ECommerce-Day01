d2376ad22b788c0d90bb90b4c6543f820970dc40
Shehari123 <rathnayakeshehari@gmail.com>
2025-03-14 10:31:06 +0000

    create Book.java for 7 question

---
+++ Book.java
@@ -0,0 +1,19 @@
+/* 7) implement classes for a Library
+There are diffrent type of books are available in the
+library like Printed books, E books, and Audio books
+
+You must use inhertance, polymorphism, encapuslation...*/
+
+public class Book{
+    private String title;
+    private String author;
+    
+    public Book(String title,string author,int year){
+        this.title = title;
+        this.author = athor;
+    }
+
+    public String getInfo(){
+        return title + "by" + author ".";
+    }
+}
